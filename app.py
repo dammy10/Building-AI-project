155e4f88487c4bc68e52c7fd3b0b066ffe6ba577
DAVID ADEDIPE <107045019+dammy10@users.noreply.github.com>
2026-06-15 09:28:47 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+def priority_score(difficulty, days_left):
+    return difficulty * 2 + (1 / days_left)
+
+
+def create_plan(topics, difficulty, days_left, hours):
+    scores = []
+
+    for i in range(len(topics)):
+        score = priority_score(difficulty[i], days_left[i])
+        scores.append((topics[i], score))
+
+    scores.sort(key=lambda x: x[1], reverse=True)
+
+    total = sum([s[1] for s in scores])
+
+    print("\n📚 Study Plan:\n")
+
+    for topic, score in scores:
+        time = (score / total) * hours
+        print(f"{topic}: {round(time, 2)} hours")
+
+
+topics = ["Math", "Physics", "History", "Biology"]
+difficulty = [5, 4, 2, 3]
+days_left = [3, 5, 10, 7]
+hours = 6
+
+create_plan(topics, difficulty, days_left, hours)
